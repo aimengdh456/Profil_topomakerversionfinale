9b939c05708c54e1b695fd3c3feca2821feec526
aimengdh456 <ia_guidouche@esi.dz>
2020-05-07 01:36:40 +0200

    some changes

---
+++ Profil_TopoM/Accueil.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Profil_TopoM
+{
+    /// <summary>
+    /// Logique d'interaction pour Accueil.xaml
+    /// </summary>
+    public partial class Accueil : UserControl
+    {
+        Trace trr = new Trace();
+        BitmapImage imgg;
+        int aym = -1;
+        SqlConnection cnx = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Fujitsu\Desktop\Profil_topo_MAKER25\Profil_topo_MAKER\Profil_TopoM\BDDtopo.mdf;Integrated Security=True");
+       
+            public Accueil()
+        {
+            InitializeComponent();
+           
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            cnx.Open();
+
+            string readString3 = "select * from Trace  where nom ='" + nomTrace.Text + "'";
+
+            SqlCommand readCommand3 = new SqlCommand(readString3, cnx);
+            int nbs = 1000;
+
+            using (SqlDataReader dataRead3 = readCommand3.ExecuteReader())
+
+            {
+                if (dataRead3 != null)
+                {
+                    while (dataRead3.Read())
+                    {
+
+                        string xas = dataRead3["Id"].ToString();
+                        nbs = int.Parse(xas);
+                        string xas2 = dataRead3["echelle"].ToString();
+                        double eche = double.Parse(xas2);
+                        string xas22 = dataRead3["echellecarte"].ToString();
+                        double echecarte = double.Parse(xas22);
+                        string xas4 = dataRead3["equidistance"].ToString();
+                        int equi = int.Parse(xas4);
+                        string xas6 = dataRead3["min"].ToString();
+                        int mi = int.Parse(xas6);
+                        string xas8 = dataRead3["max"].ToString();
+                        int ma = int.Parse(xas8);
+                        string xas10 = dataRead3["creation"].ToString();
+                        string xas105 = dataRead3["modification"].ToString();
+
+
+
+
+                        aym = nbs;
+                        string xasa = dataRead3["image"].ToString();
+                        Uri ur = new Uri(xasa);
+                        imgg = new BitmapImage(ur);
+                        Trace trace5 = new Trace(nomTrace.Text, xas10, xas105, mi, ma, eche, echecarte, equi, xasa);
+                        trr = trace5;
+                    }
+                }
+            }
+            cnx.Close();
+            if (aym == -1)
+            {
+                MessageBox.Show("il n'existe pas un tracé avec ce nom  !");
+            }
+            else
+            {
+                tracecourbes imp = new tracecourbes(imgg, trr);
+                var parent = (Grid)this.Parent;
+                parent.Children.Clear();
+                parent.Children.Add(imp);
+            }
+        }
+    }
+}
---
+++ Profil_TopoM/Classes/Trace.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profil_TopoM
+{
+    public class Trace
+    {
+
+        // private Profil profil;
+        public Trace(String name, String creat, String modif, int mini, int maxi, double echel, double echel2, int equi, String img)
+        {
+            this.nom = name;
+            this.date_creat = creat;
+            this.date_modif = modif;// pour l'affichage => DateTime.Now.ToSrting("dd/mm/yyyy hh:mm:ss")
+            this.min = mini;
+            this.max = maxi;
+            this.echelle = echel;
+            this.echellecarte = echel2;
+            this.equidistance = equi;
+            this.image = img;
+        }
+        public Trace()
+        {
+
+        }
+
+        public string nom { get; set; }
+        public string image { get; set; }
+
+        public int min { get; set; }
+        public int max { get; set; }
+        public double echelle { get; set; }
+        public double echellecarte { get; set; }
+        public int equidistance { get; set; }
+        public String date_creat { get; set; }
+        public String date_modif { get; set; }
+
+
+        // public  void modifier() { }
+        //public void  renommer(){}
+        // public void genererProfil(list< point >) {}
+
+    }
+}
